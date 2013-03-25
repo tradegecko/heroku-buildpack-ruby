68b21919731e80d3a530b40f27f5eb95129c2a6d
Bradley Priest <bradleypriest@gmail.com>
2013-03-25 13:41:34 +0800

    Upload xero certs on deploy

--- lib/language_pack.rb
+++ lib/language_pack.rb
@@ -13,7 +13,7 @@ module LanguagePack
     Instrument.instrument 'detect' do
       Dir.chdir(args.first)
 
-      pack = [ NoLockfile, Rails41, Rails4, Rails3, Rails2, Rack, Ruby ].detect do |klass|
+      pack = [ NoLockfile, Custom, Rails41, Rails4, Rails3, Rails2, Rack, Ruby ].detect do |klass|
         klass.use?
       end
 
@@ -43,3 +43,4 @@ require "language_pack/disable_deploys"
 require "language_pack/rails4"
 require "language_pack/rails41"
 require "language_pack/no_lockfile"
+require "language_pack/custom"
